cdbc908c60e27a29f69f88ec266cf6ea96df58ae
Oninnaise <Oninnaise@users.noreply.github.com>
2019-03-12 16:49:11 +0100

    Initial Commit

---
+++ Assets/VRTK/Examples/ExampleResources/Scripts/ExampleSceneSimulatorCameraRigMover.cs
@@ -0,0 +1,20 @@
+﻿namespace VRTK.Examples
+{
+    using UnityEngine;
+
+    public class ExampleSceneSimulatorCameraRigMover : MonoBehaviour
+    {
+        protected virtual void Awake()
+        {
+            Transform cameraRigTransform = transform.Find("VRSimulatorCameraRig");
+            cameraRigTransform.position -= transform.position;
+        }
+
+        private void Start()
+        {
+            Rigidbody rigidBody = gameObject.GetComponentInChildren<Rigidbody>();
+            rigidBody.isKinematic = false;
+            rigidBody.detectCollisions = true;
+        }
+    }
+}
